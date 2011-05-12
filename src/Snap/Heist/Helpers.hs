bd57ef62191e015f4a316e656d60f2afac502704
Mighty Byte <mightybyte@gmail.com>
2011-05-12 08:28:40 -0400

    Forgot import, bumped containers bounds

--- src/Snap/Heist/Helpers.hs
+++ src/Snap/Heist/Helpers.hs
@@ -9,6 +9,8 @@ Generic combinators for building splices.
 
 module Snap.Heist.Helpers where
 
+import Text.Templating.Heist
+
 conditionalSplice :: Monad m => Bool -> Splice m
 conditionalSplice False = return []
 conditionalSplice True = runChildren
